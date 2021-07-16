b14b774e17e4e3d5e01553a3395f9b1e17bb8434
bisnukundu <bisnukundu@gmail.com>
2021-07-17 03:27:59 +0600

    i have setup larave-websockets

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <form>
+
+                    <div class="d-flex">
+                        <div>
+                            <input type="text" name="message" class="form-control w-100">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script>
+        window.Echo.channel('bisnuc')
+            .listen('Bisnu', (e) => {
+                console.log(e)
+            });
+    </script>
+</body>
+
+</html>
